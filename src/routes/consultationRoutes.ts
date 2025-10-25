fec579660f4b652a4945e6f1f8fa52057083ff02
SamsonCanon <samsoncanon2018@gmail.com>
2025-10-25 13:33:48 +0100

    fix booking time

--- src/routes/consultationRoutes.ts
+++ src/routes/consultationRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { checkConsultationPaymentStatus, consultationPaymentCallback, initializeConsultation } from '../controllers/consultationController.js'
 import { checkUnSeenMessages } from '../controllers/consultation/messageController.js'
+import { getDoctorAvailability, getDoctorTimeSlotsForDate } from '../controllers/consultation/index.js'
 
 const router = Router()
 
@@ -9,6 +10,8 @@ router.get('/check-payment-status/:transactionId', checkConsultationPaymentStatu
 router.get('/call-back', consultationPaymentCallback)
 router.post('/messages/unseen/:consultationsChat/:senderId/:receiverId', checkUnSeenMessages)
 
-
+// Doctor availability endpoints
+router.get('/doctors/:doctorId/availability', getDoctorAvailability)
+router.get('/doctors/:doctorId/availability/:date', getDoctorTimeSlotsForDate)
 
 export const consultationRoutes = router
